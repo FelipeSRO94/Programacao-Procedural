5476c914e8a2b376f1dfa08e5aad6d3444759259
FelipeSRO94 <felipe.oliveirarocha@outlook.com>
2024-02-22 12:00:04 -0300

    lista 1

---
+++ Lista 1/Calculo de Area/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+        Console.WriteLine("Digite o numero da base do triangulo: ");
+        double baseTriangulo = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o numero da altura do triangulo: ");
+        double alturaTriangulo = double.Parse(Console.ReadLine());
+
+        double areaTriangulo = (baseTriangulo * alturaTriangulo)/ 2;
+
+        bool areaMaiorQue20 = areaTriangulo > 20;
+
+        Console.WriteLine("A área do triangulo é mior que 20 ? " + (areaMaiorQue20 ? "Verdadeiro" : "Falso"));
+
+        
+    }
+}
---
+++ Lista 1/Conversor de Centavos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+
+        Console.WriteLine("Digite a quantidade de centavos que deseja converter: ");
+        int dinheiroTotal = int.Parse(Console.ReadLine());
+
+        int reais = dinheiroTotal / 100;
+        int centavos = dinheiroTotal % 100;
+
+        Console.WriteLine("O total em dinheiro é: " + reais + " real(is) e " + centavos + " centavo(s)");
+    }
+
+}
