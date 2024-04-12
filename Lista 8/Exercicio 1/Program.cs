95d3809879d1f52ac7ff022797eb44e5dd6a0f79
FelipeSRO94 <felipe.oliveirarocha@outlook.com>
2024-04-12 01:59:16 -0300

    Lista 8

---
+++ Lista 8/Exercicio 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza minhaPizza;
+
+        Console.WriteLine("Insira o ID da pizza:");
+        minhaPizza.id = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Insira o sabor da pizza:");
+        minhaPizza.sabor = Console.ReadLine();
+
+        Console.WriteLine("Insira o preço da pizza:");
+        minhaPizza.preco = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Dados da pizza:");
+        Console.WriteLine("ID: " + minhaPizza.id);
+        Console.WriteLine("Sabor: " + minhaPizza.sabor);
+        Console.WriteLine("Preço: " + minhaPizza.preco.ToString("C"));
+    }
+}
---
+++ Lista 8/Exercicio 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto[] produtos = new Produto[4];
+
+        for (int i = 0; i < produtos.Length; i++)
+        {
+            Console.WriteLine("Insira o ID do produto:");
+            produtos[i].id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insira o nome do produto:");
+            produtos[i].nome = Console.ReadLine();
+
+            Console.WriteLine("Insira o preço do produto:");
+            produtos[i].preco = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("O produto está disponível em estoque? (true/false):");
+            produtos[i].disponivelEmEstoque = Convert.ToBoolean(Console.ReadLine());
+        }
+
+        Console.WriteLine("Produtos disponíveis na vitrine:");
+        foreach (var produto in produtos)
+        {
+            if (produto.disponivelEmEstoque)
+            {
+                Console.WriteLine("Nome: " + produto.nome);
+                Console.WriteLine("Preço: " + produto.preco.ToString("C"));
+            }
+        }
+    }
+}
